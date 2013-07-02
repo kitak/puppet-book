435e7efb76dce94dd2eff68c0ae45aaf86544389
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-02 18:36:14 +0900

    initial commit

---
+++ puppet/modules/td-agent/manifests/init.pp
@@ -0,0 +1,10 @@
+class td-agent {
+  include td-agent::install
+  include td-agent::config
+  include td-agent::service
+
+    Class['td-agent::install']
+  -> Class['td-agent::config']
+  ~> Class['td-agent::service']
+}
+
